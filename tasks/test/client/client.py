76cdcdc83e6909d86f7e9e65c4db82db7528386e
Quiel <ing.jeremy.quiel@gmail.com>
2026-06-20 10:02:45 -0600

    instrucciones update

---
+++ tasks/test/client/client.py
@@ -0,0 +1,26 @@
+from ..config.Socket import Socket
+import json
+
+cliente_socket = Socket.socketClient()
+cliente_socket.connect(("127.0.0.1", 5000)) # INTENTA CONECTARSE A DESDE LOCALHOST EN EL PUERTO 5000
+
+cliente = {
+    "nombre" : "Jeremy",
+    "id" : 1,
+    "estado": True,
+    "mensaje": ""
+}
+
+while True:
+    mss = input("Mensaje: ")
+    cliente["mensaje"] = mss
+    cliente_string = json.dumps(cliente)
+
+    Socket.msgSend(cliente_socket, cliente_string)
+
+    mensaje = Socket.msgRcv(cliente_socket)
+
+    print(f"Mensaje del server: \n{mensaje}")
+
+    # cliente_socket.close()
+    # print("Se cerro la conexino del cliente")
---
+++ tasks/test/server/server.py
@@ -0,0 +1,23 @@
+from ..config.Socket import Socket
+import json
+
+server_socket = Socket.socketServer("0.0.0.0", 5000)
+server_socket.listen(10) # CAPACIDAD DE CLIENTES EN COLA (deja en espera la entrada de clientes)
+
+print(f"Server en espera en {"0.0.0.0"}:{5000}")
+cliente_socket, direction = Socket.socketAccept(server_socket)
+
+while True:
+    message = Socket.msgRcv(cliente_socket)
+    json_cliente = json.loads(message)
+
+    print(f"Dato ingresado de: {json_cliente["nombre"]}")
+    print(f"Mensaje: {json_cliente["mensaje"]}")
+
+    eco = f"Nombre: {json_cliente["nombre"]} \nID: {json_cliente["id"]} \nEstado: {json_cliente["estado"]}"
+    Socket.msgSend(cliente_socket, eco)
+
+    # CERRAR LAS CONEXIONES
+    # cliente.close()
+    # server_socket.close()
+    # print("Se cerro la conexion del server")
