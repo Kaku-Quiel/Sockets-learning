1fef9f81d3ea4b8b2448064f50cbd4e9b911131e
Kaku-Quiel <ing.jeremy.quiel@gmail.com>
2026-06-24 12:45:51 -0600

    fase 5 v1.0

--- tasks/review/content/archive/blockArchive.py
+++ tasks/review/content/archive/blockArchive.py
@@ -1,6 +1,5 @@
 import hashlib
 import random
-import string
 
 
 # === CREAR ARCHIVO ===
@@ -54,57 +53,122 @@ def corruptBytes(block):
 
     return bytes(lista_bytes)
 
+def randomCorrupt(block, probabilidad):
+    if random.randint(1, probabilidad) == probabilidad:
+        return {
+            "block": corruptBytes(block),
+            "error": True
+        }
+    return {
+        "block": block,
+        "error": False
+    }
+
+def printDataBlock(data_block):
+    print(f"Bloque: {data_block['number']}")
+    print(f"  Hash: {data_block['hash'][:32]}...")
+    print(f"  init offset: {data_block['init_offset']}")
+    print(f"  end offset: {data_block['end_offset']}")
+    print(f"  size: {data_block['size']}")
+    print(f"  bits: {data_block['bits']}")
+    print(f"  status: {data_block['status']}")
+    print()
 
 def leer_bloques(archivo, tam_bloque=50):
     """Lee archivo en bloques y calcula hash de cada uno"""
     lista = []
     
-    with open(f"docs/{archivo}", "rb") as file:
+    with open(f"docs/{archivo}", "rb+") as file:
         num_block = 1
-        offset = 0
-        
+        init_offset = 0
+        end_offset = 0
+
+        errores = 0
+        intentos = 0
+        i = 0
+
         while True:
-            bloque = file.read(tam_bloque)
-            if not bloque:
+            original_bloque = file.read(tam_bloque)
+            if not original_bloque:
                 break
+            
+
+            data_random = randomCorrupt(original_bloque, 2)
+            bloque = data_random['block']
 
             hash_block = hashear_bytes(bloque)
-            
+            end_offset = end_offset + len(bloque)
+
+            if data_random['error']:
+                errores += 1
+
             # Guardar metadata
+            
             data_block = {
                 "number": num_block,
                 "hash": hash_block,
-                "init_offset": offset,
+                "init_offset": init_offset,
+                "end_offset": end_offset - 1,
                 "size": len(bloque),
-                "bits": byte_to_bits(bloque),  # Solo para mostrar
+                "bits": (bloque),  # Solo para mostrar
                 "status": ""
             }
-
-            if random.randint(1, 3) == 3:
-                bloque = corruptBytes(bloque)
             
             # Verificar integridad (siempre debe ser True aquí)
-            data_block["status"] = "success" if verify(bloque, data_block['hash']) else "error"
+            data_block["status"] = "success" if verify(original_bloque, data_block['hash']) else "error"
 
             lista.append(data_block)
-
-            print(data_block)
-            print()
             
+            while True:
+                if data_block['status'] == "error":
+                    file.seek(init_offset)
+
+                    dist = (end_offset + 1) - init_offset
+
+                    data_random = randomCorrupt(file.read(dist), 2)
+                    bloque = data_random["block"]
+
+                    i += 1
+
+                    if i >= 5:
+                        data_block["status"] = "transmition error"
+                        break
+
+                    if not data_random["error"]:
+                        data_block["hash"] = hashear_bytes(bloque)
+                        data_block['bits'] = bloque
+                        data_block["status"] = "fixed"
+
+                else:
+                    break
+            
+            intentos += i
+            i = 0
+
             num_block += 1
-            offset += len(bloque)
+            init_offset += len(bloque)
     
-    return lista
+    return {
+        "lista": lista,
+        "intentos": intentos,
+        "errores": errores
+    }
 
 # === EJECUTAR ===
+data_lectura = leer_bloques("blocks.txt", 50)
+
 print("\n" + "="*50)
 print("LEYENDO ARCHIVO EN BLOQUES")
 print("="*50 + "\n")
 
-bloques = leer_bloques("blocks.txt", 50)
+for data_block in data_lectura["lista"]:
+    printDataBlock(data_block)
+
 
 print("="*50)
 print(f"RESULTADO FINAL")
 print("="*50)
-print(f"Total bloques: {len(bloques)}")
-print(f"Total bytes: {sum(b['size'] for b in bloques)}")
+print(f"Total bloques: {len(data_lectura['lista'])}")
+print(f"Total bytes: {sum(b['size'] for b in data_lectura['lista'])}")
+print(f"Errores: {data_lectura['errores']}")
+print(f"Numero de veces que se intento corregir: {data_lectura['intentos']}")
