d7259077c871d0590b7ffbbaceeaef650307fecb
Kaku-Quiel <ing.jeremy.quiel@gmail.com>
2026-06-25 13:38:58 -0600

    fase 5 v2.0

--- tasks/review/content/archive/blockArchive.py
+++ tasks/review/content/archive/blockArchive.py
@@ -1,7 +1,6 @@
 import hashlib
 import random
 
-
 # === CREAR ARCHIVO ===
 texto = [
     b"Habia una vez una duende que se pintaba la cara\n",
@@ -27,7 +26,7 @@ def byte_to_bits(texto_bytes):
     bits = []
     for byte in texto_bytes:
         bits.append(format(byte, '08b'))
-    return ' '.join(bits)
+    return bits
 
 def hashear_bytes(datos_bytes):
     """Hashea DIRECTAMENTE los bytes (sin convertir a texto)"""
@@ -39,136 +38,209 @@ def verify(bloque, hash_guardado):
     hash_calculado = hashear_bytes(bloque)
     return hash_calculado == hash_guardado
 
-def corruptBytes(block):
+def corruptBytes(lista_bits):
+
+    block = [int(bits, 2) for bits in lista_bits]
+
     pos = random.randint(0, len(block) - 1)
 
-    lista_bytes = list(block)
+    # lista_bytes = list(block)
 
     while True:
         caracter = random.randint(0, 255)
 
-        if caracter != lista_bytes[pos]:
-            lista_bytes[pos] = caracter
+        if caracter != block[pos]:
+            block[pos] = caracter
             break
 
-    return bytes(lista_bytes)
+    return bytes(block)
 
-def randomCorrupt(block, probabilidad):
-    if random.randint(1, probabilidad) == probabilidad:
+def randomCorrupt(lista_bits, probabilidad):
+    block = bytes([int(bits, 2) for bits in lista_bits])
+
+    if random.randint(1, probabilidad) != probabilidad:
         return {
-            "block": corruptBytes(block),
-            "error": True
+            "block": block,
+            "error": False
         }
     return {
-        "block": block,
-        "error": False
+        "block": corruptBytes(lista_bits),
+        "error": True
     }
 
 def printDataBlock(data_block):
     print(f"Bloque: {data_block['number']}")
+    # print(f"  content: {randomCorrupt(data_block['bits'], 99999999999999999999999999)['block']}") # Ver mensaje explicito
     print(f"  Hash: {data_block['hash'][:32]}...")
     print(f"  init offset: {data_block['init_offset']}")
-    print(f"  end offset: {data_block['end_offset']}")
+    # print(f"  end offset: {data_block['end_offset']}")
     print(f"  size: {data_block['size']}")
-    print(f"  bits: {data_block['bits']}")
+    print(f"  bits: {data_block['bits'][0]} {data_block['bits'][1]} {data_block['bits'][2]}...")
     print(f"  status: {data_block['status']}")
     print()
 
-def leer_bloques(archivo, tam_bloque=50):
-    """Lee archivo en bloques y calcula hash de cada uno"""
-    lista = []
-    
-    with open(f"docs/{archivo}", "rb+") as file:
-        num_block = 1
-        init_offset = 0
-        end_offset = 0
+def resultado(titulo, data):
+    print(f"\n\n")
+    print(titulo, end='')
+    print("="*50)
+    for data_block in data['lista']:
+        printDataBlock(data_block)
 
-        errores = 0
-        intentos = 0
-        i = 0
+    print("="*50)
+    print("RESULTADO FINAL")
+    print("="*50)
+    print(f"Num blocks: {data['num_blocks']}")
+    print(f"Total Bytes: {data['bytes_total']}")
+    print(f"Paso por Red: {data['red']}")
 
+def fixDataBlock(data_block, read_size, status="success"):
+    errors = 0
+
+    with open("docs/blocks.txt", "rb+") as file:
         while True:
-            original_bloque = file.read(tam_bloque)
-            if not original_bloque:
+            if data_block['status'] == "success":
                 break
-            
 
-            data_random = randomCorrupt(original_bloque, 2)
-            bloque = data_random['block']
+            else:
+                file.seek(data_block['init_offset'])
+                bloque = file.read(read_size)
+                hash_block = hashear_bytes(bloque)
 
-            hash_block = hashear_bytes(bloque)
-            end_offset = end_offset + len(bloque)
-
-            if data_random['error']:
-                errores += 1
-
-            # Guardar metadata
-            
-            data_block = {
-                "number": num_block,
-                "hash": hash_block,
-                "init_offset": init_offset,
-                "end_offset": end_offset - 1,
-                "size": len(bloque),
-                "bits": (bloque),  # Solo para mostrar
-                "status": ""
-            }
-            
-            # Verificar integridad (siempre debe ser True aquí)
-            data_block["status"] = "success" if verify(original_bloque, data_block['hash']) else "error"
-
-            lista.append(data_block)
-            
-            while True:
-                if data_block['status'] == "error":
-                    file.seek(init_offset)
-
-                    dist = (end_offset + 1) - init_offset
-
-                    data_random = randomCorrupt(file.read(dist), 2)
-                    bloque = data_random["block"]
-
-                    i += 1
-
-                    if i >= 5:
-                        data_block["status"] = "transmition error"
-                        break
-
-                    if not data_random["error"]:
-                        data_block["hash"] = hashear_bytes(bloque)
-                        data_block['bits'] = bloque
-                        data_block["status"] = "fixed"
-
-                else:
+                if verify(bloque, hash_block):
+                    data_block["hash"] = hash_block
+                    data_block["bits"] = byte_to_bits(bloque)
+                    data_block['status'] = status
                     break
+
+            errors += 1
+            if errors < 100:
+                data_block["status"] = "transmition error"
+                data_block["bits"] = 0
+                data_block["hash"] = "transmition error"
+                break
+
+        return data_block
+
+def emisor(read_size = 50, mode = "normal", error_list=[], block_list=[]):
+    if mode == "normal":
+        lista = []
+        errors = 0
+
+        with open("docs/blocks.txt", "rb+") as file:
+            num_block = 1
+            init_offset = 0
+
+            while True:
+                bloque = file.read(read_size)
+                if not bloque:
+                    break
+
+                data_block = {
+                    "number": num_block,
+                    "hash": None,
+                    "bits": None,
+                    "size": len(bloque),
+                    "init_offset": init_offset,
+                    "status": ""
+                }
+
+                data_block = fixDataBlock(data_block, read_size)
+
+                lista.append(data_block)
             
-            intentos += i
-            i = 0
+                num_block += 1
+                init_offset += len(bloque)
 
-            num_block += 1
-            init_offset += len(bloque)
+        return {
+            "lista": lista,
+            "num_blocks": len(lista),
+            "bytes_total": sum(b['size'] for b in lista),
+            "red": False
+        }
     
-    return {
-        "lista": lista,
-        "intentos": intentos,
-        "errores": errores
-    }
+    if mode == "reply":
+        lista_fixed = []
 
-# === EJECUTAR ===
-data_lectura = leer_bloques("blocks.txt", 50)
+        if error_list == []:
+            return {
+                "lista": block_list,
+                "status": "success",
+                "Error": None
+                }
+        
+        if block_list == []:
+            return {
+                "lista": block_list,
+                "status": "error",
+                "Error": "E01"
+            }
 
-print("\n" + "="*50)
-print("LEYENDO ARCHIVO EN BLOQUES")
-print("="*50 + "\n")
+        for num_block in error_list:
+            data_block = block_list[num_block - 1]
 
-for data_block in data_lectura["lista"]:
-    printDataBlock(data_block)
+            if data_block["status"] == "success":
+                return {
+                    "lista": block_list,
+                    "status": "error",
+                    "Error": "E02"
+                }
+            
+            data_block = fixDataBlock(data_block, read_size, "fixed")
+
+            lista_fixed.append(data_block)
+
+        for data_block_fixed in lista_fixed:
+            for data_block in block_list:
+                if data_block['number'] == data_block_fixed['number']:
+                    data_block = data_block_fixed
+            
+
+        return {
+            "lista": block_list,
+            "status": "success",
+            "Error": None
+        }
 
 
-print("="*50)
-print(f"RESULTADO FINAL")
-print("="*50)
-print(f"Total bloques: {len(data_lectura['lista'])}")
-print(f"Total bytes: {sum(b['size'] for b in data_lectura['lista'])}")
-print(f"Errores: {data_lectura['errores']}")
-print(f"Numero de veces que se intento corregir: {data_lectura['intentos']}")
+
+def red(data_emisor):
+    for data_block in data_emisor['lista']:
+        bits = data_block["bits"]
+        
+        data_random = randomCorrupt(bits, 3)
+
+        if data_random["error"]:
+            data_block['status'] = "error"
+            data_block['hash'] = hashear_bytes(data_random['block'])
+            data_block['bits'] = byte_to_bits(data_random["block"])
+
+    data_emisor['red'] = True
+
+def receptor(data_receptor):
+
+    lista = data_receptor['lista']
+
+    lista_errores = []
+
+    for data_block in lista:
+        if data_block['status'] != "success":
+            lista_errores.append(data_block['number'])
+
+    lista = emisor(mode="reply", error_list=lista_errores, block_list=lista) # arreglar la lista
+
+    return data_receptor
+
+    
+        
+
+data_emisor = emisor()
+
+resultado("EMISOR: ", data_emisor)
+
+red(data_emisor)
+
+resultado("RED: ", data_emisor)
+
+data_receptor = receptor(data_emisor)
+
+resultado("RECEPTOR: ", data_receptor)
